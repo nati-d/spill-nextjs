ed058b3dbe52595fcdfc04bc8baf401dbda42393
Nathnael Desalegn <nathnaeldes@gmail.com>
2025-12-03 20:49:01 +0300

    Update package dependencies to include react-icons for enhanced social platform icon support; refactor social platform icon rendering to utilize new icons for Instagram, Twitter, TikTok, and Snapchat.

--- constants/social-platform-icons.tsx
+++ constants/social-platform-icons.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Instagram, Twitter, Video, Camera } from 'lucide-react';
 import { SocialPlatform } from './social-platforms';
+import { FaSnapchat, FaTiktok } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { FaInstagram } from 'react-icons/fa6';
 
 export const getSocialPlatformIcon = (platform: SocialPlatform): React.ReactNode => {
   const iconProps = { className: "h-5 w-5", strokeWidth: 2 };
   
   switch (platform.toLowerCase()) {
     case 'instagram':
-      return React.createElement(Instagram, iconProps);
+      return React.createElement(FaInstagram, iconProps);
     case 'twitter':
-      return React.createElement(Twitter, iconProps);
+      return React.createElement(FaXTwitter, iconProps);
     case 'tiktok':
-      return React.createElement(Video, iconProps);
+      return React.createElement(FaTiktok, iconProps);
     case 'snapchat':
-      return React.createElement(Camera, iconProps);
+      return React.createElement(FaSnapchat, iconProps);
     default:
       return React.createElement(Camera, iconProps);
   }
